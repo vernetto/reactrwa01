085f7ec51917e0e238d25316bada32583e749391
vernetto <publicpierre@gmail.com>
2023-04-08 21:33:49 +0200

    add

---
+++ src/Recipe.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+export default function Recipe(props) {
+  const { name, cookTime, servings, instructions, ingredients } = props;
+  return (
+    <div>
+      <div>
+        <h3>{name}</h3>
+        <div>
+          <button>Edit</button>
+          <button>Delete</button>
+        </div>
+      </div>
+      <div>
+        <span>Cook Time:</span>
+        <span>{cookTime}</span>
+      </div>
+      <div>
+        <span>Servings:</span>
+        <span>{servings}</span>
+      </div>
+      <div>
+        <span>Instructions:</span>
+        <div>{instructions}</div>
+      </div>
+      <div>
+        <span>Ingredients:</span>
+        <div>{ingredients}</div>
+      </div>
+    </div>
+  );
+}
